851ed1e2aabcd9b36312183f9aae5c5510e54013
TomTossu <tsanchez@litebox.ai>
2025-04-22 17:46:36 -0300

    Testing commit user

--- src/components/CharacterEditor/CharacterEditor.jsx
+++ src/components/CharacterEditor/CharacterEditor.jsx
@@ -35,7 +35,7 @@ function App() {
           <h1 className={styles.title}>Create your Character</h1>
           <p className={styles.description}>
             Customize your character's look and style using the
-            controls below. What sort of adventure will you embark on?{' '}
+            controls below. What sort of adventure will you embark on?
           </p>
         </header>
         <div className={styles.controlColumn}>
