31772e41b1810cfdaee8de0c585f7574332178f9
TomTossu <tsanchez@litebox.ai>
2025-04-22 17:44:42 -0300

    Initial Commit

--- src/components/CharacterEditor/CharacterEditor.jsx
+++ src/components/CharacterEditor/CharacterEditor.jsx
@@ -34,7 +34,7 @@ function App() {
         <header className={styles.header}>
           <h1 className={styles.title}>Create your Character</h1>
           <p className={styles.description}>
-            Customize your character's look and style using the
+            Customize your character's look 
             controls below. What sort of adventure will you embark on?{' '}
           </p>
         </header>
