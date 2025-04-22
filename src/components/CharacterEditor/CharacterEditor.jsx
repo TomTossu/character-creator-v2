1f01bba01dfafa4cad6bb8cda70610e538060623
TomTossu <t.sanchez.tossutti@gmail.com>
2025-04-22 17:47:35 -0300

    Testing commit user

--- src/components/CharacterEditor/CharacterEditor.jsx
+++ src/components/CharacterEditor/CharacterEditor.jsx
@@ -34,7 +34,7 @@ function App() {
         <header className={styles.header}>
           <h1 className={styles.title}>Create your Character</h1>
           <p className={styles.description}>
-            Customize your character's look and style using the
+            Customize your character's look and using the
             controls below. What sort of adventure will you embark on?
           </p>
         </header>
